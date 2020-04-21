b183d32836cc6f27db74d095de401a5be472ce11
Mikayil Gasimov <mgasimov@c11714mgasimov.shareholder.com>
2020-04-21 16:33:40 -0700

    adding time.sleep in ping.py

--- ping/ping.py
+++ ping/ping.py
@@ -3,9 +3,6 @@
 import os
 import time
 
-#listhosts = ['google.com','facebook.com','dasdasdasdasdasdas.com',]
-
-
 file = open('output.txt','w+')
 hostnames = open('hosts.txt','r')
 lines = hostnames.read().splitlines()
@@ -14,6 +11,7 @@ for host in lines:
     response = os.system("ping -c 1 " + str(host))
     if response == 0:
         file.write(str(host) + " is UP\n")
+        time.sleep(1)
     else:
         file.write(host + " is Down\n")
 
